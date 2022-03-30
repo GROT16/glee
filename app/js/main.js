0ab94b8ab3148892190ce2bb2ee28f508aaa78c8
GROT16 <grot_16@bk.ru>
2022-03-30 17:48:09 +0400

    added about, login and terms pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,35 @@
 $(function(){
+
+  $('.trendy-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3500
+  });
+
+  // var mixer = mixitup('.product')({
+
+  // });
+
+  // var mixer = mixitup('.new__items')({
+
+  // })
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+  Fancybox.bind("[data-fancybox]", {
+    // Your options go here
+  });
   
+
 });
